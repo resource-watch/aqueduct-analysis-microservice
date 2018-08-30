4160ee14afc6a51cc59838cebed604272049696e
AliyGM <alicia.arenzana@gmail.com>
2018-08-30 18:02:53 +0200

    added future prot

--- aqueduct/services/cba_defaults_service.py
+++ aqueduct/services/cba_defaults_service.py
@@ -1,5 +1,5 @@
 import os
-
+import numpy as np
 import pandas as pd
 import sqlalchemy
 from cached_property import cached_property
@@ -28,17 +28,19 @@ class CBADefaultService(object):
     def default(self):
         fids, geogunit_name, geogunit_type = pd.read_sql_query("SELECT fids, name, type FROM lookup_master where uniqueName = '{0}' ".format(self.geogunit_unique_name), self.engine).values[0]
         clim, socio, scen_abb = self.scenario
+        rps = np.array([2, 5, 10, 25, 50, 100, 250, 500, 1000])
         ##prot
         read_prot = 'precalc_agg_riverine_{0}_nosub'.format(geogunit_type).lower()
         col_prot = 'urban_damage_v2_2010_{0}_prot_avg'.format(scen_abb)
         df_prot = pd.read_sql_query("SELECT {0} FROM {1} where id like '{2}'".format(col_prot, read_prot, self.geogunit_unique_name), self.engine)
+        prot_val = 0 if df_prot.empty else df_prot.values[0].tolist()[0]
         
         ##costs
         con_itl = pd.read_sql_query("SELECT avg(construction_cost_index) FROM lookup_construction_factors_geogunit_108 where fid in ({0}) ".format(', '.join(map(str, fids))), self.engine)
 
         return [{
-            "existing_prot": 0 if df_prot.empty else df_prot.values[0].tolist()[0],
-            "future_prot": 0 if con_itl.empty else con_itl.values[0].tolist()[0],
+            "existing_prot": prot_val,
+            "future_prot": int(rps[np.where(rps > prot_val)][0]),
             "estimated_costs": 0 if con_itl.empty else con_itl.values[0].tolist()[0]
 
         }]
