9a78f0fb617ab773fa5248369dbbeb1434e28d4e
Solomon Negusse <solomon.negusse@wri.org>
2023-08-24 17:21:03 -0500

    Update python 3.6 -> 3.11 and dependencies; lint

--- aqueduct/validators.py
+++ aqueduct/validators.py
@@ -11,7 +11,7 @@ from aqueduct.routes.api import error
 
 def myCoerc(n):
     try:
-        return lambda v: None if v in ('null') else n(v)
+        return lambda v: None if v in ("null") else n(v)
     except Exception as e:
         return None
 
@@ -19,7 +19,7 @@ def myCoerc(n):
 null2int = myCoerc(int)
 null2float = myCoerc(float)
 
-to_bool = lambda v: v.lower() in ('true', '1')
+to_bool = lambda v: v.lower() in ("true", "1")
 to_lower = lambda v: v.lower()
 # to_list = lambda v: json.loads(v.lower())
 to_list = lambda v: json.loads(v)
@@ -31,85 +31,76 @@ def validate_wra_params(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         validation_schema = {
-            'wscheme': {
-                'required': True
+            "wscheme": {"required": True},
+            "geostore": {"type": "string", "required": True},
+            "analysis_type": {"type": "string", "required": True, "default": None},
+            "month": {"required": False, "default": None, "nullable": True},
+            "year": {
+                "type": "string",
+                "required": False,
+                "default": None,
+                "nullable": True,
             },
-            'geostore': {
-                'type': 'string',
-                'required': True
+            "change_type": {
+                "type": "string",
+                "required": False,
+                "default": None,
+                "nullable": True,
             },
-            'analysis_type': {
-                'type': 'string',
-                'required': True,
-                'default': None
+            "indicator": {
+                "type": "string",
+                "required": True,
+                "default": None,
+                "nullable": True,
             },
-            'month': {
-                'required': False,
-                'default': None,
-                'nullable': True
+            "scenario": {
+                "type": "string",
+                "required": False,
+                "default": None,
+                "nullable": True,
             },
-            'year': {
-                'type': 'string',
-                'required': False,
-                'default': None,
-                'nullable': True
+            "locations": {
+                "type": "string",
+                "required": True,
+                "required": False,
+                "default": None,
+                "nullable": True,
             },
-            'change_type': {
-                'type': 'string',
-                'required': False,
-                'default': None,
-                'nullable': True
+            "input_address": {
+                "type": "string",
+                "required": False,
+                "default": None,
+                "nullable": True,
             },
-            'indicator': {
-                'type': 'string',
-                'required': True,
-                'default': None,
-                'nullable': True
+            "match_address": {
+                "type": "string",
+                "required": False,
+                "default": None,
+                "nullable": True,
             },
-            'scenario': {
-                'type': 'string',
-                'required': False,
-                'default': None,
-                'nullable': True
+            "ids": {
+                "type": "string",
+                "required": False,
+                "nullable": True,
+                "default": None,
             },
-            'locations': {
-                'type': 'string',
-                'required': True,
-                'required': False,
-                'default': None,
-                'nullable': True
-            },
-            'input_address': {
-                'type': 'string',
-                'required': False,
-                'default': None,
-                'nullable': True
-            },
-            'match_address': {
-                'type': 'string',
-                'required': False,
-                'default': None,
-                'nullable': True
-            },
-            'ids': {
-                'type': 'string',
-                'required': False,
-                'nullable': True,
-                'default': None
-            }
-
         }
 
-        jsonRequestContent = request.json or {}
+        jsonRequestContent = {}
+        if request.method == "POST":
+            jsonRequestContent = request.get_json()
         rArgs = {**request.args, **jsonRequestContent}
         kwargs.update(rArgs)
-        logging.debug(f'[MIDDLEWARE - ws scheme]: {kwargs}')
+        logging.debug(f"[MIDDLEWARE - ws scheme]: {kwargs}")
         logging.debug(f"[VALIDATOR - wra_weights]: {kwargs}")
+        print("func args 3", kwargs)
         validator = Validator(validation_schema, allow_unknown=True)
 
         if not validator.validate(kwargs):
+            print("func args 2", kwargs)
             return error(status=400, detail=validator.errors)
-        kwargs['sanitized_params'] = validator.normalized(kwargs)
+        kwargs["sanitized_params"] = validator.normalized(kwargs)
+        print("func args 2", kwargs)
         return func(*args, **kwargs)
 
     return wrapper
@@ -121,113 +112,119 @@ def validate_params_cba(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         validation_schema = {
-            'geogunit_unique_name': {'type': 'string', 'required': True},
-            'existing_prot': {
-                'type': 'integer',
-                'required': False,
-                'coerce': null2int,
-                'default': None,
-                'nullable': True,
-                'min': 0,
-                'max': 1000
+            "geogunit_unique_name": {"type": "string", "required": True},
+            "existing_prot": {
+                "type": "integer",
+                "required": False,
+                "coerce": null2int,
+                "default": None,
+                "nullable": True,
+                "min": 0,
+                "max": 1000,
             },
-            'scenario': {
-                'type': 'string',
-                'required': True,
-                'allowed': ["business as usual", "pessimistic", "optimistic", "rcp4p5", "rcp8p5"],
-                'coerce': to_lower
+            "scenario": {
+                "type": "string",
+                "required": True,
+                "allowed": [
+                    "business as usual",
+                    "pessimistic",
+                    "optimistic",
+                    "rcp4p5",
+                    "rcp8p5",
+                ],
+                "coerce": to_lower,
             },
-            'prot_fut': {
-                'type': 'integer',
-                'required': False,
-                'coerce': null2int,
-                'default': None,
-                'nullable': True,
-                'min': 0,
-                'max': 1000
+            "prot_fut": {
+                "type": "integer",
+                "required": False,
+                "coerce": null2int,
+                "default": None,
+                "nullable": True,
+                "min": 0,
+                "max": 1000,
             },
-            'implementation_start': {
-                'type': 'integer',
-                'required': True,
-                'coerce': int,
-                'min': 2020,
-                'max': 2079
+            "implementation_start": {
+                "type": "integer",
+                "required": True,
+                "coerce": int,
+                "min": 2020,
+                "max": 2079,
             },
-            'implementation_end': {
-                'type': 'integer',
-                'required': True,
-                'coerce': int,
-                'min': 2021,
-                'max': 2080
+            "implementation_end": {
+                "type": "integer",
+                "required": True,
+                "coerce": int,
+                "min": 2021,
+                "max": 2080,
             },
-            'infrastructure_life': {
-                'type': 'integer',
-                'required': True,
-                'coerce': int,
-                'min': 1,
-                'max': 100
+            "infrastructure_life": {
+                "type": "integer",
+                "required": True,
+                "coerce": int,
+                "min": 1,
+                "max": 100,
             },
-            'benefits_start': {
-                'type': 'integer',
-                'required': True,
-                'coerce': int,
-                'min': 2020,
-                'max': 2080
+            "benefits_start": {
+                "type": "integer",
+                "required": True,
+                "coerce": int,
+                "min": 2020,
+                "max": 2080,
             },
-            'ref_year': {
-                'type': 'integer',
-                'required': True,
-                'coerce': int,
-                'allowed': [2030, 2050, 2080]
+            "ref_year": {
+                "type": "integer",
+                "required": True,
+                "coerce": int,
+                "allowed": [2030, 2050, 2080],
             },
-            'estimated_costs': {
-                'type': 'float',
-                'required': False,
-                'coerce': null2float,
-                'default': None,
-                'nullable': True,
-                'min': 0,
-                'max': 1000
+            "estimated_costs": {
+                "type": "float",
+                "required": False,
+                "coerce": null2float,
+                "default": None,
+                "nullable": True,
+                "min": 0,
+                "max": 1000,
             },
-            'discount_rate': {
-                'type': 'float',
-                'required': True,
-                'coerce': float,
-                'min': 0,
-                'max': 1
+            "discount_rate": {
+                "type": "float",
+                "required": True,
+                "coerce": float,
+                "min": 0,
+                "max": 1,
             },
-            'om_costs': {
-                'type': 'float',
-                'required': True,
-                'coerce': float,
-                'min': 0,
-                'max': 1
+            "om_costs": {
+                "type": "float",
+                "required": True,
+                "coerce": float,
+                "min": 0,
+                "max": 1,
             },
-            'user_urb_cost': {
-                'type': 'float',
-                'required': False,
-                'coerce': null2float,
-                'default': None,
-                'nullable': True,
-                'min': 0,
-                'max': 1000
+            "user_urb_cost": {
+                "type": "float",
+                "required": False,
+                "coerce": null2float,
+                "default": None,
+                "nullable": True,
+                "min": 0,
+                "max": 1000,
             },
-            'user_rur_cost': {
-                'type': 'float',
-                'required': False,
-                'coerce': null2float,
-                'default': None,
-                'nullable': True,
-                'min': 0,
-                'max': 1000
-            }
+            "user_rur_cost": {
+                "type": "float",
+                "required": False,
+                "coerce": null2float,
+                "default": None,
+                "nullable": True,
+                "min": 0,
+                "max": 1000,
+            },
         }
-        
+
         validator = Validator(validation_schema, allow_unknown=True)
-        if not validator.validate(kwargs['params']):
+        if not validator.validate(kwargs["params"]):
             return error(status=400, detail=validator.errors)
 
-        kwargs['sanitized_params'] = validator.normalized(kwargs['params'])
+        kwargs["sanitized_params"] = validator.normalized(kwargs["params"])
         logging.debug(f"[VALIDATOR - cba_params]: {kwargs['sanitized_params']}")
         return func(*args, **kwargs)
 
@@ -240,33 +237,39 @@ def validate_params_cba_def(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         validation_schema = {
-            'geogunit_unique_name': {'type': 'string', 'required': True},
-            'scenario': {
-                'type': 'string',
-                'required': True,
-                'allowed': ["business as usual", "pessimistic", "optimistic", "rcp4p5", "rcp8p5"],
-                'coerce': to_lower
+            "geogunit_unique_name": {"type": "string", "required": True},
+            "scenario": {
+                "type": "string",
+                "required": True,
+                "allowed": [
+                    "business as usual",
+                    "pessimistic",
+                    "optimistic",
+                    "rcp4p5",
+                    "rcp8p5",
+                ],
+                "coerce": to_lower,
             },
-            'flood': {
-                'type': 'string',
-                'required': False,
-                'coerce': to_lower,
-                'default': 'riverine',
-                'allowed': ["riverine", "coastal"],
+            "flood": {
+                "type": "string",
+                "required": False,
+                "coerce": to_lower,
+                "default": "riverine",
+                "allowed": ["riverine", "coastal"],
             },
-            'sub_scenario': {
-                'type': 'boolean',
-                'required': False,
-                'default': False,
-                'coerce': (str, to_bool)
-            }
+            "sub_scenario": {
+                "type": "boolean",
+                "required": False,
+                "default": False,
+                "coerce": (str, to_bool),
+            },
         }
         logging.debug(f"[VALIDATOR - cba_def_params]: {kwargs}")
         validator = Validator(validation_schema, allow_unknown=True)
-        if not validator.validate(kwargs['params']):
+        if not validator.validate(kwargs["params"]):
             return error(status=400, detail=validator.errors)
 
-        kwargs['sanitized_params'] = validator.normalized(kwargs['params'])
+        kwargs["sanitized_params"] = validator.normalized(kwargs["params"])
         logging.debug(f"[VALIDATOR - cba_def_params]: {kwargs['sanitized_params']}")
         return func(*args, **kwargs)
 
@@ -279,47 +282,43 @@ def validate_params_risk(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         validation_schema = {
-            'geogunit_unique_name': {'type': 'string', 'required': True},
-            'existing_prot': {
-                'type': 'integer',
-                'required': False,
-                'coerce': null2int,
-                'default': None,
-                'nullable': True,
-                'min': 0,
-                'max': 1000
+            "geogunit_unique_name": {"type": "string", "required": True},
+            "existing_prot": {
+                "type": "integer",
+                "required": False,
+                "coerce": null2int,
+                "default": None,
+                "nullable": True,
+                "min": 0,
+                "max": 1000,
             },
-            'scenario': {
-                'type': 'string',
-                'required': True,
-                'allowed': ["business as usual", "pessimistic", "optimistic", "rcp4p5", "rcp8p5"],
-                'coerce': to_lower
-
+            "scenario": {
+                "type": "string",
+                "required": True,
+                "allowed": [
+                    "business as usual",
+                    "pessimistic",
+                    "optimistic",
+                    "rcp4p5",
+                    "rcp8p5",
+                ],
+                "coerce": to_lower,
             },
-            'sub_scenario': {
-                'type': 'boolean',
-                'required': True,
-                'coerce': (str, to_bool)
+            "sub_scenario": {
+                "type": "boolean",
+                "required": True,
+                "coerce": (str, to_bool),
             },
-            'exposure': {
-                'type': 'string',
-                'required': True,
-                'coerce': to_lower
-            },
-            'flood': {
-                'type': 'string',
-                'required': True,
-                'coerce': to_lower
-            }
-
+            "exposure": {"type": "string", "required": True, "coerce": to_lower},
+            "flood": {"type": "string", "required": True, "coerce": to_lower},
         }
 
         validator = Validator(validation_schema, allow_unknown=True)
-        if not validator.validate(kwargs['params']):
+        if not validator.validate(kwargs["params"]):
             logging.debug(f"[VALIDATOR - risk_params]: {kwargs}")
             return error(status=400, detail=validator.errors)
 
-        kwargs['sanitized_params'] = validator.normalized(kwargs['params'])
+        kwargs["sanitized_params"] = validator.normalized(kwargs["params"])
 
         return func(*args, **kwargs)
 
